2ce48b0dba08e0b5721b76fd4d9b9bf39dbd918b
imagire <imagire@gmail.com>
2025-05-10 18:36:33 +0900

    add files
    3目に変更

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# tick-tack-toe
-ゲームプログラム論の課題です
+# 課題：強くして
+本リポジトリのAIに対して、何らかの工夫をしてください！
+
+* 計算を効率化する
+* 新たなアルゴリズムを実装する
+
+5目並べにすると、わかりやすいかもしれません。
+
+![結果画像](image.png)
+
+# 工夫点
+
+* 【行ったことをここに書く】
+
+# 取り組み方
+* 本プロジェクトをforkして、取り組んでください。
+* GitHub Actions (Actionsのタブ)を機能させて、README.mdに記述された下記のバッチの「tpu-game-2025」を自分のアカウントに差し替えてください。
+* readme.md に実施した工夫を記載してください
+* 可能であれば、速度等を計測して、具体的な効率化度合い、強さを示してください。
+* 納得できるところまでできたところでプルリクを出してください。
+
+[![MSBuild](https://github.com/tpu-game-2025/tick-tack-toe/actions/workflows/msbuild.yml/badge.svg)](https://github.com/tpu-game-2025/tick-tack-toe/actions/workflows/msbuild.yml)
+
+（↑のソースコードの「tpu-game-2025」を自分のアカウント名に差し替えてください（２か所））
---
+++ src/tick-tack-toe/tick-tack-toe.cpp
@@ -0,0 +1,303 @@
+﻿#include <memory>
+#include <iostream>
+
+
+class Mass {
+public:
+	enum status {
+		BLANK,
+		PLAYER,
+		ENEMY,
+	};
+private:
+	status s_ = BLANK;
+public:
+	status getStatus() const { return s_; }
+
+	bool put(status s) {
+		if (s_ != BLANK) return false;
+		s_ = s;
+		return true;
+	}
+};
+
+class Board;
+
+class AI {
+public:
+	AI() {}
+	virtual ~AI() {}
+
+	virtual bool think(Board& b) = 0;
+
+public:
+	enum type {
+		TYPE_ORDERED = 0,
+	};
+
+	static AI* createAi(type type);
+};
+
+// 順番に打ってみる
+class AI_ordered : public AI {
+public:
+	AI_ordered() {}
+	~AI_ordered() {}
+
+	bool think(Board& b);
+};
+
+AI* AI::createAi(type type)
+{
+	switch (type) {
+	case TYPE_ORDERED:
+		return new AI_ordered();
+	default:
+		return new AI_ordered();
+		break;
+	}
+
+	return nullptr;
+}
+
+class Board
+{
+	friend class AI_ordered;
+
+public:
+	enum WINNER {
+		NOT_FINISED = 0,
+		PLAYER,
+		ENEMY,
+		DRAW,
+	};
+private:
+	enum {
+		BOARD_SIZE = 3,
+	};
+	Mass mass_[BOARD_SIZE][BOARD_SIZE];
+
+public:
+	Board() {
+	}
+	Board::WINNER calc_result() const
+	{
+		// 縦横斜めに同じキャラが入っているか検索
+		// 横
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			Mass::status winner = mass_[y][0].getStatus();
+			if (winner != Mass::PLAYER && winner != Mass::ENEMY) continue;
+			int x = 1;
+			for (; x < BOARD_SIZE; x++) {
+				if (mass_[y][x].getStatus() != winner) break;
+			}
+			if (x == BOARD_SIZE) { return (Board::WINNER)winner; }
+		}
+		// 縦
+		for (int x = 0; x < BOARD_SIZE; x++) {
+			Mass::status winner = mass_[0][x].getStatus();
+			if (winner != Mass::PLAYER && winner != Mass::ENEMY) continue;
+			int y = 1;
+			for (; y < BOARD_SIZE; y++) {
+				if (mass_[y][x].getStatus() != winner) break;
+			}
+			if (y == BOARD_SIZE) { return(Board::WINNER) winner; }
+		}
+		// 斜め
+		{
+			Mass::status winner = mass_[0][0].getStatus();
+			if (winner == Mass::PLAYER || winner == Mass::ENEMY) {
+				int idx = 1;
+				for (; idx < BOARD_SIZE; idx++) {
+					if (mass_[idx][idx].getStatus() != winner) break;
+				}
+				if (idx == BOARD_SIZE) { return (Board::WINNER)winner; }
+			}
+		}
+		{
+			Mass::status winner = mass_[BOARD_SIZE - 1][0].getStatus();
+			if (winner == Mass::PLAYER || winner == Mass::ENEMY) {
+				int idx = 1;
+				for (; idx < BOARD_SIZE; idx++) {
+					if (mass_[BOARD_SIZE - 1 - idx][idx].getStatus() != winner) break;
+				}
+				if (idx == BOARD_SIZE) { return (Board::WINNER)winner; }
+			}
+		}
+		// 上記勝敗がついておらず、空いているマスがなければ引分け
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			for (int x = 0; x < BOARD_SIZE; x++) {
+				Mass::status fill = mass_[y][x].getStatus();
+				if (fill == Mass::BLANK) return NOT_FINISED;
+			}
+		}
+		return DRAW;
+	}
+
+	bool put(int x, int y) {
+		if (x < 0 || BOARD_SIZE <= x ||
+			y < 0 || BOARD_SIZE <= y) return false;// 盤面外
+		return mass_[y][x].put(Mass::PLAYER);
+	}
+
+	void show() const {
+		std::cout << "　　";
+		for (int x = 0; x < BOARD_SIZE; x++) {
+			std::cout << " " << x + 1 << "　";
+		}
+		std::cout << "\n　";
+		for (int x = 0; x < BOARD_SIZE; x++) {
+			std::cout << "＋－";
+		}
+		std::cout << "＋\n";
+		for (int y = 0; y < BOARD_SIZE; y++) {
+			std::cout << " " << char('a' + y);
+			for (int x = 0; x < BOARD_SIZE; x++) {
+				std::cout << "｜";
+				switch (mass_[y][x].getStatus()) {
+				case Mass::PLAYER:
+					std::cout << "〇";
+					break;
+				case Mass::ENEMY:
+					std::cout << "×";
+					break;
+				case Mass::BLANK:
+					std::cout << "　";
+					break;
+				default:
+//					if (mass_[y][x].isListed(Mass::CLOSE)) std::cout << "＋"; else
+//					if (mass_[y][x].isListed(Mass::OPEN) ) std::cout << "＃"; else
+					std::cout << "　";
+				}
+			}
+			std::cout << "｜\n";
+			std::cout << "　";
+			for (int x = 0; x < BOARD_SIZE; x++) {
+				std::cout << "＋－";
+			}
+			std::cout << "＋\n";
+		}
+	}
+};
+
+bool AI_ordered::think(Board& b)
+{
+	for (int y = 0; y < Board::BOARD_SIZE; y++) {
+		for (int x = 0; x < Board::BOARD_SIZE; x++) {
+			if (b.mass_[y][x].put(Mass::ENEMY)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+class Game
+{
+private:
+	const AI::type ai_type = AI::TYPE_ORDERED;
+
+	Board board_;
+	Board::WINNER winner_ = Board::NOT_FINISED;
+	AI* pAI_ = nullptr;
+
+public:
+	Game() {
+		pAI_ = AI::createAi(ai_type);
+	}
+	~Game() {
+		delete pAI_;
+	}
+
+	bool put(int x, int y) {
+		bool success = board_.put(x, y);
+		if (success) winner_ = board_.calc_result();
+
+		return success;
+	}
+
+	bool think() {
+		bool success = pAI_->think(board_);
+		if (success) winner_ = board_.calc_result();
+		return success;
+	}
+
+	Board::WINNER is_finised() {
+		return winner_;
+	}
+
+	void show() {
+		board_.show();
+	}
+};
+
+
+
+
+void show_start_message()
+{
+	std::cout << "========================" << std::endl;
+	std::cout << "       GAME START       " << std::endl;
+	std::cout << std::endl;
+	std::cout << "input position likes 1 a" << std::endl;
+	std::cout << "========================" << std::endl;
+}
+
+void show_end_message(Board::WINNER winner)
+{
+	if (winner == Board::PLAYER) {
+		std::cout << "You win!" << std::endl;
+	}
+	else if (winner == Board::ENEMY)
+	{
+		std::cout << "You lose..." << std::endl;
+	}
+	else {
+		std::cout << "Draw" << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	for (;;) {// 無限ループ
+		show_start_message();
+
+		// initialize
+		bool player_turn = true;
+		std::shared_ptr<Game> game(new Game());
+
+		while (1) {
+			game->show();// 盤面表示
+
+			// 勝利判定
+			Board::WINNER winner = game->is_finised();
+			if (winner) {
+				show_end_message(winner);
+				break;
+			}
+
+			if (player_turn) {
+				// user input
+				char col[1], row[1];
+				do {
+					std::cout << "? ";
+					std::cin >> row >> col;
+				} while (!game->put(row[0] - '1', col[0] - 'a'));
+			}
+			else {
+				// AI
+				if (!game->think()) {
+					show_end_message(Board::WINNER::PLAYER);// 投了
+				}
+				std::cout << std::endl;
+			}
+			// プレイヤーとAIの切り替え
+			player_turn = !player_turn;
+		}
+	}
+
+	return 0;
+}
